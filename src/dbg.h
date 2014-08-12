b101f9f4c2753e9ac73887e8f4684d78c33e58ea
Mike Jarvis <michael@jarvis.net>
2014-08-12 15:59:18 -0400

    Update license to remove the 3rd clause

--- src/dbg.h
+++ src/dbg.h
@@ -1,3 +1,16 @@
+/* Copyright (c) 2003-2014 by Mike Jarvis
+ *
+ * TreeCorr is free software: redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions, and the disclaimer given in the accompanying LICENSE
+ *    file.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions, and the disclaimer given in the documentation
+ *    and/or other materials provided with the distribution.
+ */
 //---------------------------------------------------------------------------
 #ifndef dbgH
 #define dbgH

59e213fa1d83d5422be12999f8979ee9ae5157f6
Mike Jarvis <michael@jarvis.net>
2014-08-12 15:59:18 -0400

    Update license to remove the 3rd clause

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -10,9 +10,6 @@
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions, and the disclaimer given in the documentation
 #    and/or other materials provided with the distribution.
-# 3. Neither the name of the {organization} nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
 
 
 def get_aardvark():

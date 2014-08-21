bf3233e9d0e76f8d4ee1249f7e267bb6c6cd9833
Mike Jarvis <michael@jarvis.net>
2014-08-20 23:28:23 -0400

    Update version to 3.0.0

--- treecorr/__init__.py
+++ treecorr/__init__.py
@@ -12,7 +12,7 @@
 #    and/or other materials provided with the distribution.
 
 
-version = '3.0beta'
+version = '3.0.0'
 
 from .celestial import CelestialCoord, angle_units, arcsec, arcmin, degrees, hours, radians
 from .config import read_config

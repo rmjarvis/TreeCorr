f290e12647849ee85f6fc66fc15634ca127cfc4e
Mike Jarvis <michael@jarvis.net>
2014-08-28 10:08:39 -0400

    Try various compiler options if comp_type is unknown. #7

--- setup.py
+++ setup.py
@@ -31,26 +31,29 @@ copt =  {
     'gcc' : ['-fopenmp','-O3','-ffast-math'],
     'icc' : ['-openmp','-O3'],
     'clang' : ['-O3','-ffast-math'],
+    'unknown' : [],
 }
 lopt =  {
     'gcc' : ['-fopenmp'],
     'icc' : ['-openmp'],
     'clang' : [],
+    'unknown' : [],
 }
+
 
 def get_compiler(cc):
     """Try to figure out which kind of compiler this really is.
     In particular, try to distinguish between clang and gcc, either of which may
     be called cc or gcc.
     """
-    cmd = cc + ' --version 2>&1'
+    cmd = [cc,'--version']
     import subprocess
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     lines = p.stdout.readlines()
     if 'clang' in lines[0]:
         # Supposedly, clang will support openmp in version 3.5.  Let's go with that for now...
-        # If the version is reports >= 3.5, let's call in gcc, rather than clang to get
-        # the gcc -fopenmp flag.
+        # If the version is reports >= 3.5, let's call it gcc, rather than clang to get
+        # the -fopenmp flag.
         line = lines[1]
         import re
         match = re.search(r'[0-9]+(\.[0-9]+)+', line)
@@ -73,7 +76,101 @@ def get_compiler(cc):
     elif 'icc' in cc or 'icpc' in cc:
         return 'icc'
     else:
+        # OK, the main thing we need to know is what openmp flag we need for this compiler,
+        # so let's just try the various options and see what works.  Don't try icc, since 
+        # the -openmp flag there gets treated as '-o penmp' by gcc and clang, which is bad.
+        # Plus, icc should be detected correctly by the above procedure anyway.
+        for cc_type in ['gcc', 'clang']:
+            if try_cc(cc, cc_type):
+                return cc_type
+        # I guess none of them worked.  Now we really do have to bail.
         return 'unknown'
+
+def try_cc(cc, cc_type):
+    """
+    If cc --version is not helpful, the last resort is to try each compiler type and see
+    if it works.
+    """
+    cpp_code = """
+#include <iostream>
+#include <vector>
+#ifdef _OPENMP
+#include "omp.h"
+#endif
+
+int get_max_threads() {
+#ifdef _OPENMP
+    return omp_get_max_threads();
+#else
+    return 1;
+#endif
+}
+
+int main() {
+    int n = 500;
+    std::vector<double> x(n,0.);
+
+#ifdef _OPENMP
+#pragma omp parallel for schedule(static)
+#endif
+    for (int i=0; i<n; ++i) x[i] = 2*i+1;
+
+    double sum = 0.;
+    for (int i=0; i<n; ++i) sum += x[i];
+    // Sum should be n^2 = 250000
+
+    std::cout<<get_max_threads()<<"  "<<sum<<std::endl;
+    return 0;
+}
+"""
+    cpp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.cpp')
+    cpp_file.write(cpp_code)
+    cpp_file.close()
+
+    # Just get a named temporary file to write to:
+    o_file = tempfile.NamedTemporaryFile(delete=False, suffix='.os')
+    o_file.close()
+
+    # Try compiling with the given flags
+    import subprocess
+    cmd = [cc] + copt[cc_type] + ['-c',cpp_file.name,'-o',o_file.name]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    lines = p.stdout.readlines()
+    p.communicate()
+    if p.returncode != 0:
+        os.remove(cpp_file.name)
+        if os.path.exists(o_file.name): os.remove(o_file.name)
+        return False
+
+    # Another named temporary file for the executable
+    exe_file = tempfile.NamedTemporaryFile(delete=False, suffix='.exe')
+    exe_file.close()
+
+    # Try linking
+    cmd = [cc] + lopt[cc_type] + [o_file.name,'-o',exe_file.name]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    lines = p.stdout.readlines()
+    p.communicate()
+
+    if p.returncode and cc == 'cc':
+        # The linker needs to be a c++ linker, which isn't 'cc'.  However, I couldn't figure
+        # out how to get setup.py to tell me the actual command to use for linking.  All the
+        # executables available from build_ext.compiler.executables are 'cc', not 'c++'.
+        # I think this must be related to the bugs about not handling c++ correctly.
+        #    http://bugs.python.org/issue9031
+        #    http://bugs.python.org/issue1222585
+        # So just switch it manually and see if that works.
+        cmd = ['c++'] + lopt[cc_type] + [o_file.name,'-o',exe_file.name]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        lines = p.stdout.readlines()
+        p.communicate()
+
+    # Remove the temp files
+    os.remove(cpp_file.name)
+    os.remove(o_file.name)
+    if os.path.exists(exe_file.name): os.remove(exe_file.name)
+    return p.returncode == 0
+
 
 # This was supposed to remove the -Wstrict-prototypes flag
 # But it doesn't work....
@@ -90,14 +187,13 @@ class my_builder( build_ext ):
     def build_extensions(self):
         cc = self.compiler.executables['compiler_cxx'][0]
         comp_type = get_compiler(cc)
-        print 'Using compiler %s, which is %s'%(cc,comp_type)
-        if copt.has_key(comp_type):
-            for e in self.extensions:
-                e.extra_compile_args = copt[ comp_type ]
-        if lopt.has_key(comp_type):
-            for e in self.extensions:
-                e.extra_link_args = lopt[ comp_type ]
+        if cc == comp_type:
+            print 'Using compiler %s'%(cc)
+        else:
+            print 'Using compiler %s, which is %s'%(cc,comp_type)
         for e in self.extensions:
+            e.extra_compile_args = copt[ comp_type ]
+            e.extra_link_args = lopt[ comp_type ]
             e.include_dirs = ['include']
         build_ext.build_extensions(self)
 

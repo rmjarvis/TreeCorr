3193bf5569477484c7c181419256b83d07a8708a
Mike Jarvis <michael@jarvis.net>
2019-03-26 17:49:58 -0400

    Workaround for cffi bug causing occasional lock freeze

--- treecorr/ngcorrelation.py
+++ treecorr/ngcorrelation.py
@@ -99,7 +99,8 @@ class NGCorrelation(treecorr.BinnedCorr2):
         # rather than being able to rely on the Python memory manager.
         # In case __init__ failed to get that far
         if hasattr(self,'corr'):  # pragma: no branch
-            treecorr._lib.DestroyCorr2(self.corr, self._d1, self._d2, self._bintype)
+            if not treecorr._ffi._lock.locked(): # pragma: no branch
+                treecorr._lib.DestroyCorr2(self.corr, self._d1, self._d2, self._bintype)
 
     def __eq__(self, other):
         return (isinstance(other, NGCorrelation) and
--- treecorr/nkcorrelation.py
+++ treecorr/nkcorrelation.py
@@ -96,7 +96,8 @@ class NKCorrelation(treecorr.BinnedCorr2):
         # rather than being able to rely on the Python memory manager.
         # In case __init__ failed to get that far
         if hasattr(self,'corr'):  # pragma: no branch
-            treecorr._lib.DestroyCorr2(self.corr, self._d1, self._d2, self._bintype)
+            if not treecorr._ffi._lock.locked(): # pragma: no branch
+                treecorr._lib.DestroyCorr2(self.corr, self._d1, self._d2, self._bintype)
 
     def __eq__(self, other):
         return (isinstance(other, NKCorrelation) and

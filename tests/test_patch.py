Adjust the sample variance forula slightly to a more correct use of w in the normalization

tests/test_patch.py
@@ -1227,7 +1227,7 @@ def test_nn_jk():
     print('Time to calculate sample covariance = ',t1-t0)
     print('varxi = ',cov3b.diagonal())
     print('ratio = ',cov3b.diagonal() / var_xib)
-    np.testing.assert_allclose(cov3b.diagonal(), var_xib, rtol=0.5*tol_factor)
+    np.testing.assert_allclose(cov3b.diagonal(), var_xib, rtol=0.4*tol_factor)
 
     # Check NN cross-correlation and other combinations of dr, rd.
     rn3 = treecorr.NNCorrelation(bin_size=0.3, min_sep=10., max_sep=30., var_method='jackknife')

treecorr/binnedcorr2.py
@@ -920,9 +920,14 @@ def _cov_sample(corrs):
     w = np.sum(wlist,axis=0)
     w /= np.sum(w)  # Now w is the fractional weight for each patch
 
-    vmean = np.mean(v, axis=0)
-    v -= vmean
-    C = 1./(npatch-1) * (w * v.T).dot(v)
+    sumvw = np.sum(v * w[:,np.newaxis])
+    sumw = np.sum(w)
+    sumww = np.sum(w**2)
+    meanv = sumvw / sumw
+    meanw = sumw / npatch
+    v -= meanv
+    #C = 1./(npatch-1) * (w * v.T).dot(v)  # if all w are ==, equivalent to this.
+    C = meanw / (sumw - sumww/sumw) * (w * v.T).dot(v)
     return C
 
 def _cov_marked(corrs):
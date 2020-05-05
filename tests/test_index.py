749f775f197eb247af84bec25b289b776d519aa1
Mike Jarvis <michael@jarvis.net>
2020-05-05 13:03:13 -0400

    Use timeit for more reliable timing tests of count_near and get_near

--- tests/test_index.py
+++ tests/test_index.py
@@ -18,6 +18,7 @@ import coord
 import gc
 import treecorr
 import platform
+import timeit
 
 from test_helper import CaptureLog, assert_raises, timer
 
@@ -50,23 +51,18 @@ def test_count_near():
     # The simplest way to trigger the build is to ask for ntop
     field.nTopLevelNodes
 
-    t0 = time.time()
     n1 = np.sum((x-x0)**2 + (y-y0)**2 < sep**2)
-    t1 = time.time()
+    t1 = min(timeit.repeat(lambda: np.sum((x-x0)**2 + (y-y0)**2 < sep**2),number=100))
     n2 = field.count_near(x=x0, y=y0, sep=sep)
-    t2 = time.time()
-    n3 = field.count_near(x=x0, y=y0, sep=sep)
-    t3 = time.time()
-    print('n1 = ',n1,'  time = ',t1-t0)
-    print('n2 = ',n2,'  time = ',t2-t1)
-    print('n3 = ',n3,'  time = ',t3-t2)
-    assert n2 == n1
-    assert n3 == n1
+    t2 = min(timeit.repeat(lambda: field.count_near(x=x0, y=y0, sep=sep), number=100))
+    print('n1 = ',n1,'  time = ',t1)
+    print('n2 = ',n2,'  time = ',t2)
+    assert n1 == n2
     print('implementation is ',platform.python_implementation())
     if platform.python_implementation() != 'PyPy':
         # The JIT in PyPy can sometimes beat the tree code.
-        assert t2-t1 < t1-t0
-        assert t3-t2 < t1-t0
+        # Do best of two timing test to avoid random occasional slowness.
+        assert t2 < t1
 
     # Check G and K with other allowed argument patterns.
     kfield = cat.getKField(min_size=0.01, max_size=sep, min_top=5)
@@ -87,23 +83,22 @@ def test_count_near():
     field = cat.getNField()
     field.nTopLevelNodes
 
-    t0 = time.time()
     n1 = np.sum((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < sep**2)
-    t1 = time.time()
+    t1 = min(timeit.repeat(lambda: np.sum((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < sep**2), number=100))
     n2 = field.count_near(x=x0, y=y0, z=z0, sep=sep)
-    t2 = time.time()
+    t3 = min(timeit.repeat(lambda: field.count_near(x=x0, y=y0, z=z0, sep=sep), number=100))
     c = coord.CelestialCoord.from_xyz(x0,y0,z0)
     r0 = np.sqrt(x0**2+y0**2+z0**2)
     n3 = field.count_near(ra=c.ra, dec=c.dec, r=r0, sep=sep)
-    t3 = time.time()
-    print('n1 = ',n1,'  time = ',t1-t0)
-    print('n2 = ',n2,'  time = ',t2-t1)
-    print('n3 = ',n3,'  time = ',t3-t2)
+    t3 = min(timeit.repeat(lambda: field.count_near(ra=c.ra, dec=c.dec, r=r0, sep=sep), number=100))
+    print('n1 = ',n1,'  time = ',t1)
+    print('n2 = ',n2,'  time = ',t2)
+    print('n3 = ',n3,'  time = ',t3)
     assert n2 == n1
     assert n3 == n1
     if platform.python_implementation() != 'PyPy':
-        assert t2-t1 < t1-t0
-        assert t3-t2 < t1-t0
+        assert t2 < t1
+        assert t3 < t1
 
     # Check G and K with other allowed argument patterns.
     kfield = cat.getKField(min_size=0.01, max_size=sep, min_top=5)
@@ -142,22 +137,23 @@ def test_count_near():
     c = coord.CelestialCoord.from_xyz(x0,y0,z0)
     x0,y0,z0 = c.get_xyz()
     r0 = 2 * np.sin(sep/2)  # length of chord subtending 0.1 radians.
-    t0 = time.time()
     n1 = np.sum((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < r0**2)
-    t1 = time.time()
+    t1 = min(timeit.repeat(lambda: np.sum((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < r0**2), number=100))
     n2 = field.count_near(c, sep=sep*coord.radians)
-    t2 = time.time()
+    t2 = min(timeit.repeat(lambda: field.count_near(c, sep=sep*coord.radians), number=100))
     n3 = field.count_near(ra=c.ra.rad, dec=c.dec.rad, ra_units='radians', dec_units='radians',
                           sep=sep * coord.radians)
-    t3 = time.time()
-    print('n1 = ',n1,'  time = ',t1-t0)
-    print('n2 = ',n2,'  time = ',t2-t1)
-    print('n3 = ',n3,'  time = ',t3-t2)
+    t3 = min(timeit.repeat(lambda: field.count_near(ra=c.ra.rad, dec=c.dec.rad, ra_units='radians',
+                                                    dec_units='radians', sep=sep * coord.radians),
+                           number=100))
+    print('n1 = ',n1,'  time = ',t1)
+    print('n2 = ',n2,'  time = ',t2)
+    print('n3 = ',n3,'  time = ',t3)
     assert n2 == n1
     assert n3 == n1
     if platform.python_implementation() != 'PyPy':
-        assert t2-t1 < t1-t0
-        assert t3-t2 < t1-t0
+        assert t2 < t1
+        assert t3 < t1
 
     # Check G and K with other allowed argument patterns.
     kfield = cat.getKField(min_size=0.01, max_size=0.1, min_top=5)
@@ -214,18 +210,18 @@ def test_get_near():
 
     t0 = time.time()
     i1 = np.where(((x-x0)**2 + (y-y0)**2 < sep**2))[0]
-    t1 = time.time()
+    t1 = min(timeit.repeat(lambda: np.where(((x-x0)**2 + (y-y0)**2 < sep**2))[0], number=100))
     i2 = field.get_near(x=x0, y=y0, sep=sep)
-    t2 = time.time()
+    t2 = min(timeit.repeat(lambda: field.get_near(x=x0, y=y0, sep=sep), number=100))
     i3 = field.get_near(x0, y0, sep)
-    t3 = time.time()
-    print('i1 = ',i1[:20],'  time = ',t1-t0)
-    print('i2 = ',i2[:20],'  time = ',t2-t1)
-    print('i3 = ',i3[:20],'  time = ',t3-t2)
+    t3 = min(timeit.repeat(lambda: field.get_near(x0, y0, sep), number=100))
+    print('i1 = ',i1[:20],'  time = ',t1)
+    print('i2 = ',i2[:20],'  time = ',t2)
+    print('i3 = ',i3[:20],'  time = ',t3)
     np.testing.assert_array_equal(i2, i1)
     np.testing.assert_array_equal(i3, i1)
-    #assert t2-t1 < t1-t0  # These don't always pass.  The tree version is usually a bit faster,
-    #assert t3-t2 < t1-t0  # but not by much and not always.  So don't require it in unit test.
+    #assert t2 < t1    # These don't always pass.  The tree version is usually a faster,
+    #assert t3 < t1    # but not always.  So don't require it in unit test.
 
     # Invalid ways to specify x,y,sep
     assert_raises(TypeError, field.get_near)
@@ -260,20 +256,21 @@ def test_get_near():
 
     t0 = time.time()
     i1 = np.where(((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < sep**2))[0]
-    t1 = time.time()
+    t1 = min(timeit.repeat(lambda: np.where(((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < sep**2))[0],
+                           number=100))
     i2 = field.get_near(x=x0, y=y0, z=z0, sep=sep)
-    t2 = time.time()
+    t2 = min(timeit.repeat(lambda: field.get_near(x=x0, y=y0, z=z0, sep=sep), number=100))
     c = coord.CelestialCoord.from_xyz(x0,y0,z0)
     r0 = np.sqrt(x0**2+y0**2+z0**2)
     i3 = field.get_near(ra=c.ra, dec=c.dec, r=r0, sep=sep)
-    t3 = time.time()
-    print('i1 = ',i1[:20],'  time = ',t1-t0)
-    print('i2 = ',i2[:20],'  time = ',t2-t1)
-    print('i3 = ',i3[:20],'  time = ',t3-t2)
+    t3 = min(timeit.repeat(lambda: field.get_near(ra=c.ra, dec=c.dec, r=r0, sep=sep), number=100))
+    print('i1 = ',i1[:20],'  time = ',t1)
+    print('i2 = ',i2[:20],'  time = ',t2)
+    print('i3 = ',i3[:20],'  time = ',t3)
     np.testing.assert_array_equal(i2, i1)
     np.testing.assert_array_equal(i3, i1)
-    #assert t2-t1 < t1-t0
-    #assert t3-t2 < t1-t0
+    #assert t2 < t1
+    #assert t3 < t1
 
     # Invalid ways to specify x,y,z,sep
     ra0 = c.ra / coord.degrees
@@ -348,19 +345,22 @@ def test_get_near():
     r0 = 2 * np.sin(sep / 2)  # length of chord subtending sep radians.
     t0 = time.time()
     i1 = np.where(((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < r0**2))[0]
-    t1 = time.time()
+    t1 = min(timeit.repeat(lambda: np.where(((x-x0)**2 + (y-y0)**2 + (z-z0)**2 < r0**2))[0],
+                           number=100))
     i2 = field.get_near(c, sep=sep, sep_units='rad')
-    t2 = time.time()
+    t2 = min(timeit.repeat(lambda: field.get_near(c, sep=sep, sep_units='rad'), number=100))
     i3 = field.get_near(ra=c.ra.rad, dec=c.dec.rad, ra_units='radians', dec_units='radians',
                         sep=sep * coord.radians)
-    t3 = time.time()
-    print('i1 = ',i1[:20],'  time = ',t1-t0)
-    print('i2 = ',i2[:20],'  time = ',t2-t1)
-    print('i3 = ',i3[:20],'  time = ',t3-t2)
+    t3 = min(timeit.repeat(lambda: field.get_near(ra=c.ra.rad, dec=c.dec.rad, ra_units='radians',
+                                                  dec_units='radians', sep=sep * coord.radians),
+                           number=100))
+    print('i1 = ',i1[:20],'  time = ',t1)
+    print('i2 = ',i2[:20],'  time = ',t2)
+    print('i3 = ',i3[:20],'  time = ',t3)
     np.testing.assert_array_equal(i2, i1)
     np.testing.assert_array_equal(i3, i1)
-    #assert t2-t1 < t1-t0
-    #assert t3-t2 < t1-t0
+    #assert t2 < t1
+    #assert t3 < t1
 
     # Invalid ways to specify ra,dec,sep
     assert_raises(TypeError, field.get_near)

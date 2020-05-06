03473bc88dc158625dd92dda08801991a80bf7be
Joe Zuntz <joezuntz@googlemail.com>
2020-05-06 14:59:34 +0100

    add more to catalog_formats

--- treecorr/catalog_formats.py
+++ treecorr/catalog_formats.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 class FitsReader:
+    subgroup_type = int
+    subgroup_default = 1
     def __init__(self, file_name):
         import fitsio
         self.file = fitsio.FITS(file_name, 'r')
@@ -21,21 +23,35 @@ class FitsReader:
         self.file.close()
 
 class HdfReader:
+    subgroup_type = str
+    subgroup_default = '/'
     def __init__(self, file_name):
         import h5py
         self.file = h5py.File(file_name, 'r')
 
-    def read(self, group, cols, s):
-        if np.isscalar(cols):
-            return self.file[group][cols][s]
+    def _group(self, group):
+        # get a group from a name, using
+        # the root if the group is empty
+        if group == '':
+            g = self.file['/']
         else:
-            return {col: self.file[group][col][s] for col in cols}
+            g = self.file[group]
+        return g
+
+
+    def read(self, group, cols, s):
+        g = self._group(group)
+
+        if np.isscalar(cols):
+            return g[cols][s]
+        else:
+            return {col: g[col][s] for col in cols}
 
     def row_count(self, group, col):
-        return self.file[group][col].size
+        return self._group(group)[col].size
 
     def names(self, group):
-        return list(self.file[group].keys())
+        return list(self._group(group).keys())
 
     def __enter__(self):
         return self

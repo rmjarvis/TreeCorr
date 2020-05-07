4496d08b0161f9695d634efbd0277cc793149e3d
Joe Zuntz <joezuntz@googlemail.com>
2020-05-07 14:46:19 +0100

    add more tests

--- tests/test_catalog_formats.py
+++ tests/test_catalog_formats.py
@@ -3,7 +3,7 @@ from test_helper import get_from_wiki, assert_raises
 import os
 import numpy as np
 
-def _test_reader(file_name, reader_class, ext, bad_ext='invalid'):
+def _test_reader(file_name, reader_class, ext, def_ext, bad_ext='invalid'):
     get_from_wiki(file_name)
     file_name = os.path.join('data',file_name)
     reader = reader_class(file_name)
@@ -12,27 +12,44 @@ def _test_reader(file_name, reader_class, ext, bad_ext='invalid'):
     assert_raises(ValueError, reader.check_valid_ext, 'invalid2')
     reader.check_valid_ext(ext)
 
+    assert reader.choose_extension({}, 'ext', 0) == def_ext
+
 
     s = slice(0, 10, 2)
     data = reader.read(ext, ['RA'], s)
+    dec = reader.read(ext, 'DEC', s)
     assert data['RA'].size == 5
+    assert dec.size == 5
     assert reader.row_count(ext, 'RA') == 390935
     assert reader.row_count(ext, 'GAMMA1') == 390935
 
     assert set(reader.names(ext)) == set("INDEX RA DEC Z EPSILON GAMMA1 GAMMA2 KAPPA MU".split())
     return reader
 
+def test_context():
+    with FitsReader(os.path.join('data','Aardvark.fit')) as infile:
+        pass
+
+
 def test_fits_reader():
-    r = _test_reader('Aardvark.fit', FitsReader, 1, 0)
+    r = _test_reader('Aardvark.fit', FitsReader, 1, 1, 0)
     # check we can also index by integer, not just number
     d = r.read('AARDWOLF', ['DEC'], np.arange(10))
+    assert r.choose_extension({}, 'g1_ext', 0) == 1
+    assert r.choose_extension({'g1_ext':'gg1'}, 'g1_ext', 0, 'ext') == 'gg1'
+    assert r.choose_extension({'g1_ext':'gg1'}, 'g2_ext', 0, 0) == 0
     assert d.size==10
 
 def test_hdf_reader():
-    _test_reader('Aardvark.hdf5', HdfReader, '/')
+    r = _test_reader('Aardvark.hdf5', HdfReader, '/', '/')
+    r = _test_reader('Aardvark.hdf5', HdfReader, '', '/')
+    assert r.choose_extension({'g1_ext': 'g1'}, 'g1_ext', 0) == 'g1'
+    assert r.choose_extension({}, 'g1_ext', 0, 'ext') == 'ext'
+
 
 
 if __name__ == '__main__':
 
     test_fits_reader()
     test_hdf_reader()
+    test_context()

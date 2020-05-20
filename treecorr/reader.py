a2b206523cc0ee2420792da73e82c4d7cf5d3f18
Mike Jarvis <michael@jarvis.net>
2020-05-19 20:41:46 -0400

    Add AsciiReader.  So far working enough to replace _check_ascii

--- treecorr/reader.py
+++ treecorr/reader.py
@@ -29,10 +29,199 @@ both provide dicts.
 import numpy as np
 from .config import get_from_list
 
+class AsciiReader:
+    """Reader interface for ASCII files.
+    Uses pandas if possible, else numpy.
+    """
+    can_slice = True
+    col_type = int
+
+    def __init__(self, file_name, comment_marker='#', delimiter=None):
+        self.file_name = file_name
+        self.comment_marker = comment_marker
+        self.delimiter = delimiter
+
+    def __contains__(self, ext):
+        """Check if there is an extension with the given name in the file.
+
+        Parameters
+        ----------
+        ext: str
+            name or index to search for
+
+        Returns
+        -------
+        bool
+            Whether the extension exists
+        """
+        # None is the only valid "extension" for ASCII files
+        return ext is None
+
+    def check_valid_ext(self, ext):
+        """Check if an extension is valid for reading, and raise ValueError if not.
+
+        None is the only valid extension for ASCII files.
+
+        Parameters
+        ----------
+        ext: str
+            The extension to check
+        """
+        if ext is not None:
+            raise ValueError("Invalid ext={} for file {}".format(ext,self.file_name))
+
+    @property
+    def data(self):
+        if self._data is not None:
+            return self._data
+        if self.ncols is None:
+            raise RuntimeError('Cannot read when not in a "with" context')
+
+        # I want read_csv to ignore header lines that start with the comment marker, but
+        # there is currently a bug in read_csv that messing things up when we do this.
+        # cf. https://github.com/pydata/pandas/issues/4623
+        # For now, my workaround in to count how many lines start with the comment marker
+        # and skip them by hand.
+        skiprows = 0
+        with open(file_name, 'r') as fid:
+            for line in fid:  # pragma: no branch
+                if line.startswith(comment_marker): skiprows += 1
+                else: break
+        skiprows += self.start
+        if self.end is None:
+            nrows = None
+        else:
+            nrows = self.end - self.start
+        if self.every_nth != 1:
+            start = skiprows
+            skiprows = lambda x: x < start or (x-start) % self.every_nth != 0
+            nrows = (nrows-1) // self.every_nth + 1
+        try:
+            import pandas
+            if delimiter is None:
+                data = pandas.read_csv(file_name, comment=comment_marker, delim_whitespace=True,
+                                       header=None, skiprows=skiprows, nrows=nrows)
+            else:
+                data = pandas.read_csv(file_name, comment=comment_marker, delimiter=delimiter,
+                                       header=None, skiprows=skiprows, nrows=nrows)
+            data = data.dropna(axis=0).values
+        except ImportError:
+            self.logger.warning("Unable to import pandas..  Using np.genfromtxt instead.\n"+
+                                "Installing pandas is recommended for increased speed when "+
+                                "reading ASCII catalogs.")
+            if self.every_nth == 1:
+                data = np.genfromtxt(file_name, comments=comment_marker, delimiter=delimiter,
+                                     skip_header=skiprows, max_rows=nrows)
+            else:
+                # Numpy can't handle skiprows being a function.  Have to do this manually.
+                data = np.genfromtxt(file_name, comments=comment_marker, delimiter=delimiter,
+                                     skip_header=start, max_rows=self.end - self.start)
+                data = data[::self.every_nth]
+
+        # If only one row, and not using pands, then the shape comes in as one-d.  Reshape it:
+        if len(data.shape) == 1:
+            data = data.reshape(1,-1)
+
+        self._data = data
+        return self._data
+
+    def read(self, ext, cols, s):
+        """Read a slice of a column or list of columns from a specified extension.
+
+        Parameters
+        ----------
+        ext: str
+            Ignored
+        cols: str/list
+            The name(s) of column(s) to read  (Only 1 column name allowed here.)
+        s: slice/array
+            A slice object or selection of integers to read
+
+        Returns
+        -------
+        data: array
+            The data that is read.
+        """
+        return self.data[:,cols-1][s]
+
+    def row_count(self, ext, col):
+        """Count the number of rows in the named extension and column
+
+        Unlike in FitsReader, col is required.
+
+        Parameters
+        ----------
+        ext: str
+            The HDF group name to use
+        col: str
+            The column to use
+
+        Returns
+        -------
+        count: int
+        """
+        return self.data.shape[0]
+
+    def names(self, ext):
+        """Return a list of the names of all the columns in an extension
+
+        Parameters
+        ----------
+        ext: str
+            The extension to search for columns
+
+        Returns
+        -------
+        names: list
+            A list of string column names
+        """
+        return range(1,self.ncols+1)
+
+    def __enter__(self):
+        # Context manager, enables "with AsciiReader(filename) as f:"
+
+        # Do a trivial read of 1 row, just to get basic info about columns
+        data = np.genfromtxt(self.file_name, comments=self.comment_marker, delimiter=self.delimiter,
+                             max_rows=1)
+        if len(data.shape) != 1:  # pragma: no cover
+            raise IOError('Unable to parse the input catalog as a numpy array')
+        self.ncols = data.shape[0]
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self._data = None
+
+    def choose_extension(self, config, name, num, default=None):
+        """Select an extension name or index from a configuration.
+
+        Only None is valid, so this always returns None.
+
+        Parameters
+        ----------
+        config: dict
+            config to choose from
+        name: str
+            parameter name to get
+        num: int
+            if the value is a list, which item to get
+        default: str
+            optional, the fall-back if not found
+
+        Returns
+        -------
+        ext: str
+            The HDF group name
+        """
+        return None
+
+
 class FitsReader:
     """Reader interface for FITS files.
     Uses fitsio to read columns, etc.
     """
+    col_type = str
+
     def __init__(self, file_name):
         """Open a file
 
@@ -197,6 +386,7 @@ class HdfReader:
     """
     # h5py can always accept slices as indices
     can_slice = True
+    col_type = str
 
     def __init__(self, file_name):
         import h5py  # Just to check right away that it will work.

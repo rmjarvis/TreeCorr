30076dd1a3f3d23cda5a1f63aedcb0dbcaac0bc2
Mike Jarvis <michael@jarvis.net>
2020-05-19 22:46:22 -0400

    Refactor reader tests

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -19,55 +19,63 @@ import treecorr
 from treecorr.reader import FitsReader, HdfReader
 from test_helper import get_from_wiki, assert_raises, assert_warns, timer
 
-def _test_reader(file_name, reader_class, ext, def_ext, bad_ext='invalid'):
-    get_from_wiki(file_name)
-    file_name = os.path.join('data',file_name)
-    reader = reader_class(file_name)
-
-    assert_raises(ValueError, reader.check_valid_ext, bad_ext)
-    assert_raises(ValueError, reader.check_valid_ext, 'invalid2')
-    reader.check_valid_ext(ext)
-
-    assert reader.choose_extension({}, 'ext', 0) == def_ext
-
-
-    s = slice(0, 10, 2)
-    data = reader.read(ext, ['RA'], s)
-    dec = reader.read(ext, 'DEC', s)
-    assert data['RA'].size == 5
-    assert dec.size == 5
-    assert reader.row_count(ext, 'RA') == 390935
-    assert reader.row_count(ext, 'GAMMA1') == 390935
-
-    assert set(reader.names(ext)) == set("INDEX RA DEC Z EPSILON GAMMA1 GAMMA2 KAPPA MU".split())
-    return reader
-
-
 @timer
 def test_fits_reader():
-    r = _test_reader('Aardvark.fit', FitsReader, 'AARDWOLF', 1)
-    r = _test_reader('Aardvark.fit', FitsReader, 1, 1, 0)
-    # check we can also index by integer, not just number
-    d = r.read('AARDWOLF', ['DEC'], np.arange(10))
-    assert r.choose_extension({}, 'g1_ext', 0) == 1
-    assert r.choose_extension({'g1_ext':'gg1'}, 'g1_ext', 0, 'ext') == 'gg1'
-    assert r.choose_extension({'g1_ext':'gg1'}, 'g2_ext', 0, 0) == 0
-    assert d.size==10
+    get_from_wiki('Aardvark.fit')
+    with FitsReader(os.path.join('data','Aardvark.fit')) as r:
+
+        assert_raises(ValueError, r.check_valid_ext, 'invalid')
+        assert_raises(ValueError, r.check_valid_ext, 0)
+        r.check_valid_ext('AARDWOLF')
+        r.check_valid_ext(1)
+
+        # Default ext is 1
+        assert r.choose_extension({}, 'ext', 0) == 1
+
+        s = slice(0, 10, 2)
+        for ext in [1, 'AARDWOLF']:
+            data = r.read(ext, ['RA'], s)
+            dec = r.read(ext, 'DEC', s)
+            assert data['RA'].size == 5
+            assert dec.size == 5
+
+            assert r.row_count(ext, 'RA') == 390935
+            assert r.row_count(ext, 'GAMMA1') == 390935
+            assert set(r.names(ext)) == set("INDEX RA DEC Z EPSILON GAMMA1 GAMMA2 KAPPA MU".split())
+
+        # check we can also index by integer, not just number
+        d = r.read('AARDWOLF', ['DEC'], np.arange(10))
+        assert r.choose_extension({}, 'g1_ext', 0) == 1
+        assert r.choose_extension({'g1_ext':'gg1'}, 'g1_ext', 0, 'ext') == 'gg1'
+        assert r.choose_extension({'g1_ext':'gg1'}, 'g2_ext', 0, 0) == 0
+        assert d.size==10
 
 
 @timer
 def test_hdf_reader():
-    r = _test_reader('Aardvark.hdf5', HdfReader, '/', '/')
-    r = _test_reader('Aardvark.hdf5', HdfReader, '', '/')
-    assert r.choose_extension({'g1_ext': 'g1'}, 'g1_ext', 0) == 'g1'
-    assert r.choose_extension({}, 'g1_ext', 0, 'ext') == 'ext'
+    get_from_wiki('Aardvark.hdf5')
+    with HdfReader(os.path.join('data','Aardvark.hdf5')) as r:
 
+        assert_raises(ValueError, r.check_valid_ext, 'invalid')
+        r.check_valid_ext('/')
+        r.check_valid_ext('')
 
-@timer
-def test_context():
-    get_from_wiki('Aardvark.fit')
-    with FitsReader(os.path.join('data','Aardvark.fit')) as infile:
-        pass
+        # Default ext is '/'
+        assert r.choose_extension({}, 'ext', 0) == '/'
+
+        s = slice(0, 10, 2)
+        data = r.read('/', ['RA'], s)
+        dec = r.read('/', 'DEC', s)
+        assert data['RA'].size == 5
+        assert dec.size == 5
+
+        assert r.row_count('/', 'RA') == 390935
+        assert r.row_count('/', 'GAMMA1') == 390935
+        assert set(r.names('/')) == set("INDEX RA DEC Z EPSILON GAMMA1 GAMMA2 KAPPA MU".split())
+
+        assert r.choose_extension({'g1_ext': 'g1'}, 'g1_ext', 0) == 'g1'
+        assert r.choose_extension({}, 'g1_ext', 0, 'ext') == 'ext'
+
 
 @timer
 def test_can_slice():
@@ -91,5 +99,4 @@ def test_can_slice():
 if __name__ == '__main__':
     test_fits_reader()
     test_hdf_reader()
-    test_context()
     test_can_slice()

94d25a43c45dd0290994be7f01f2a337656207f9
Mike Jarvis <michael@jarvis.net>
2021-03-08 21:57:06 -0500

    Simplify how we keep track of npatch

--- treecorr/nkcorrelation.py
+++ treecorr/nkcorrelation.py
@@ -372,10 +372,10 @@ class NKCorrelation(treecorr.BinnedCorr2):
         self._rk = None
 
         if not isinstance(cat1,list):
-            self.npatch1 = cat1._npatch
+            self.npatch1 = cat1.npatch
             cat1 = cat1.get_patches(low_mem=low_mem)
         if not isinstance(cat2,list):
-            self.npatch2 = cat2._npatch
+            self.npatch2 = cat2.npatch
             cat2 = cat2.get_patches(low_mem=low_mem)
 
         vark = treecorr.calculateVarK(cat2)

202b5f58637f270264cab1df63ddf4f7fe91542e
Mike Jarvis <michael@jarvis.net>
2022-04-07 18:40:21 -0400

    Don't need print_function anymore

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -11,7 +11,6 @@
 #    this list of conditions, and the disclaimer given in the documentation
 #    and/or other materials provided with the distribution.
 
-from __future__ import print_function
 import logging
 import sys
 import os

48c59319018ad237ba901a4d0e10cfa51eaca6b2
Mike Jarvis <michael@jarvis.net>
2022-10-07 16:07:21 -0400

    Add set_max_omp_threads function

--- treecorr/__init__.py
+++ treecorr/__init__.py
@@ -48,7 +48,7 @@ _lib = _ffi.dlopen(lib_file)
 Rperp_alias = 'FisherRperp'
 
 from .config import read_config
-from .util import set_omp_threads, get_omp_threads
+from .util import set_omp_threads, get_omp_threads, set_max_omp_threads
 from .catalog import Catalog, read_catalogs, calculateVarG, calculateVarK
 from .binnedcorr2 import BinnedCorr2, estimate_multi_cov, build_multi_cov_design_matrix
 from .ggcorrelation import GGCorrelation

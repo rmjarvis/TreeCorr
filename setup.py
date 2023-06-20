7d6f44f831549f4d98ab9ef1e632ce0d3a326c1f
Mike Jarvis <michael@jarvis.net>
2023-06-20 00:10:32 -0400

    Get rid of unused *_C.h files

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ scripts = ['corr2', 'corr3']
 scripts = [ os.path.join('scripts',f) for f in scripts ]
 
 sources = glob.glob(os.path.join('src','*.cpp'))
-if os.name != 'nt': sources.remove(os.path.join('src','windows.cpp'))
 
 headers = glob.glob(os.path.join('include','*.h'))
 

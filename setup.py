362429ae7189ea1987790c7e3f57faa45ec08c2c
Mike Jarvis <michael@jarvis.net>
2024-10-17 12:26:34 -0400

    Make sure python libdir is included in library_dirs for linking (#162)

--- setup.py
+++ setup.py
@@ -405,9 +405,16 @@ def fix_compiler(compiler):
 class my_builder( build_ext ):
 
     def finalize_options(self):
+        from distutils import sysconfig
+
         super().finalize_options()
 
         self.include_dirs.append('include')
+
+        # Add the system libdir if it exists.
+        libdir = sysconfig.get_config_var('LIBDIR')
+        if libdir is not None:
+            self.library_dirs.append(libdir)
 
         # Add pybind11's include dir
         # GalSim has a whole long thing for this.

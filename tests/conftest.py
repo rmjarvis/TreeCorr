d998a9a1901a6d7fa530a5e135d13a11e1041850
Mike Jarvis <michael@jarvis.net>
2025-09-08 15:51:05 -0400

    Let pytest run from root dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,10 @@
+import pytest
+
 def pytest_collectstart(collector):
     if hasattr(collector, 'skip_compare'):
         collector.skip_compare += 'stderr',
+
+# cf. https://stackoverflow.com/questions/62044541/change-pytest-working-directory-to-test-case-directory
+@pytest.fixture(autouse=True)
+def change_test_dir(request, monkeypatch):
+    monkeypatch.chdir(request.fspath.dirname)
